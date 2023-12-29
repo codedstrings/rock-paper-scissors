6d1a47b91675e9433ff1830d9bab63ac0070066b
codedstrings <mridhulml@gmail.com>
2023-12-29 18:00:27 +0530

    complete console based game

--- script.js
+++ script.js
@@ -9,42 +9,89 @@ function getComputerChoice(){
 // console.log(getComputerChoice());
 function playRound(playerSelection, computerSelection) {
     // your code here!
+    let winner=0; //0 equals tie, 
+                  //1 equals player won
+                  //2 equals computer won.
     switch (playerSelection) {
         case "rock":
             if(computerSelection=="scissor"){
-                return "You win! " +playerSelection +" beats "+computerSelection;
+                console.log("You win! " +playerSelection +" beats "+computerSelection);
+                winner=1;
             }
             else if(computerSelection=="paper"){
-                return "You lose. "+computerSelection+" beats "+playerSelection;
+                console.log("You lose. "+computerSelection+" beats "+playerSelection);
+                winner=2;
             }
-            else return "its a tie!";
-            // break;
+            else console.log("its a tie!");
+            break;
         case "paper":
             if(computerSelection=="rock"){
-                return "You win! " +playerSelection +" beats "+computerSelection;
+                console.log("You win! " +playerSelection +" beats "+computerSelection);
+                winner=1;
             }
             else if(computerSelection=="scissor"){
-                return "You lose. "+computerSelection+" beats "+playerSelection;
+                console.log("You lose. "+computerSelection+" beats "+playerSelection);
+                winner=2
             }
-            else return "its a tie!";
+            else console.log("its a tie!");
+            // else return "its a tie!";
             break;
         case "scissor":
             if(computerSelection=="paper"){
-                return "You win! " +playerSelection +" beats "+computerSelection;
+                console.log("You win! " +playerSelection +" beats "+computerSelection); 
+                winner=1;
             }
             else if(computerSelection=="rock"){
-                return "You lose. "+computerSelection+" beats "+playerSelection;
+                console.log("You lose. "+computerSelection+" beats "+playerSelection);
+                winner=2;
             }
-            else return "its a tie!";
+            else console.log("its a tie!");
+            // else return "its a tie!";
             break;
         default:
             break;
     }
-    // else if(playerSelection==sci){}
+    return winner;
   }
 
-  const playerSelection = prompt("rock, paper, or scissor?").toLowerCase();
-  const computerSelection = getComputerChoice();
+function game(){
+    let round=0;
+    let playerscore=0;
+    let computerscore=0;
+
+    //game plays till round becomes 5.
+    while(round<5){
+        console.log("Round"+(round+1));
+        //get userinput and convert into lowercase to be caseinsensitve.
+        let playerSelection = prompt("rock, paper, or scissor?").toLowerCase();
+        const computerSelection = getComputerChoice();
+            console.log("You: "+playerSelection+", computer: "+computerSelection);
+
+        //if winner=1 player wins, winner=2 computer wins, and winner=0 is tie
+        let winner=playRound(playerSelection,computerSelection);
+
+        //update scoreboard and round based on the winner.
+        if(winner==1){
+            playerscore++;
+            // round++;
+        }
+        else if(winner==2){
+            computerscore++;
+            // round++;
+        }
+        //nothing is done for a tie. ie winner==0;
+        console.log("scoreboard: you-"+playerscore+", computer-"+computerscore);
+        round++;
+    }
+
+    //printing the winner to the console.
+    if(playerscore==computerscore) console.log("It's a tie, play again");
+    else if(playerscore>computerscore) console.log("You win the game!");
+    else console.log("You lost to a computer!")
+
+}
+
+ game();
 //   const computerSelection="scissor"
-  console.log("You: "+playerSelection+", computer: "+computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+
+//   console.log(playRound(playerSelection, computerSelection));
